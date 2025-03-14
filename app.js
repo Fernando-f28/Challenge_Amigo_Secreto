df530c31b40326f9faa76d3e1f7c067b4091c46d
Fernando <mfernando.ferrer28@gmail.com>
2025-03-13 21:07:25 -0600

    Actualizar CSS

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Variables
-let amigo = document.getElementById("amigo");
+let nombre = document.getElementById("amigo");
 console.log(listaAmigos);
 let amigoSecreto = [];
 let ulamigo = document.getElementById("listaAmigos");
 //Funciones
 function agregar_amigo () {
-    let nombre = document.getElementById("Amigo")
+    let nombre = document.getElementById("amigo")
     let amigo = nombre.Value;
     if ('amigo != amigo'){
         alert("Debes ingresar un Nomnbre")
